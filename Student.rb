78250b7e9c3ae428058220c1490c66d917dc1e57
James Serengia <jamesserengia@gmail.com>
2023-04-11 04:27:48 +0300

    Add student class

--- Student.rb
+++ Student.rb
@@ -1,2 +1,12 @@
-class Student
+require "./Person"
+
+class Student < Person
+  def initialize(age, name, parent_permission, classroom)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
