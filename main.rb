0bbeaa8dd741ac7e901a72fea9f475d7289a5006
James Serengia <jamesserengia@gmail.com>
2023-04-14 09:35:30 +0300

    Add get user input fuctionality

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
-require './person'
-require './capitalize_decorator'
-require './trimmer_decorator'
+#! /usr/bin/env ruby
+require "app"
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_erson.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+def main
+  puts "Welcome to School Library App!\n\n"
+  app = App.new()
+  app.run
+end
+
+main
